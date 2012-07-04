e117b24e4f56e2f3942950fd594d17213d1fb764
John Perkins <jeperkins4@gmail.com>
2012-07-03 23:23:20 -0400

    Updates for Sabre 2.0

--- lib/sabre/reservation.rb
+++ lib/sabre/reservation.rb
@@ -1,36 +1,34 @@
 module Sabre
   class Reservation
-    def self.book(session, chain_code, hotel_code, unit_count, guest_count, line_number, amount, currency, name, card_code, card_number, expire_date, start_date, end_date )
-      client = Sabre.client('OTA_HotelResLLS1.4.1RQ.wsdl')
-      response = client.request(:ota_hotel_res_rq, Sabre.request_header('1.4.1')) do
+    def self.book(session, chain_code, hotel_code, unit_count, guest_count, line_number, amount, currency, name, card_code, card_number, expire_date, check_in, check_out, confirmation_number )
+      client = Sabre.client('OTA_HotelResLLS2.0.0RQ.wsdl')
+      response = client.request(:ota_hotel_res_rq, Sabre.request_header('2.0.0')) do
         Sabre.namespaces(soap)
         soap.header = session.header('Hotel Booking','sabreXML','OTA_HotelResLLSRQ')
         soap.body = {
-          'POS' => Sabre.pos,
-             'HotelReservations' => {
-              'HotelReservation' => {
-                'RoomStays' => { 
-                  'RoomStay' => {
-                    'RoomTypes' => {
-                      'RoomType' => '', :attributes! => { 'RoomType' => { 'NumberOfUnits' => unit_count } } 
-                    }, 
-                    'BasicPropertyInfo' => { 'Line' => '', :attributes! => { 'Line' => { 'Number' => line_number } } }
-                  }
+          'Hotel' => {
+            'BasicPropertyInfo' => { 'ConfirmationNumber' => '' },
+            'Guarantee' => {
+              'CC_Info' => {
+                'PaymentCard' => '',
+                'PersonName' => {
+                  'Surname' => name
                 }, 
-                'Profiles' => {
-                  'Customer' => { 
-                     'PaymentForm' => { 
-                        'PaymentCard' => { 
-                          'Guarantee' => {
-                            'PersonName' => {
-                              'Surname' => name
-                            }
-                          }, :attributes! => { 'Guarantee' => { 'Code' => 'G' } } 
-                        }, :attributes! => { 'PaymentCard' => { 'CardCode' => card_code, 'CardNumber' => card_number, 'ExpireDate' => expire_date.strftime('%Y-%m') } } 
-                     } 
-                  }   
-                }
+                :attributes! => { 'PaymentCard' => { 'Code' => card_code, 'ExpireDate' => expire_date.strftime('%Y-%m'), 'Number' => card_number } } 
+
               }
+            },
+            #'GuestCounts' => '',
+            #'POS' => Sabre.pos,
+            'RoomType' => '',
+            #'TimeSpan' => '',
+            :attributes! => { 
+              'BasicPropertyInfo' => { 'RPH' => line_number }, 
+            #  'TimeSpan' => { 'Start' => check_in.strftime('%m-%d'), 'End' => check_out.strftime('%m-%d') }, 
+              'Guarantee' => { 'Type' => 'GDPST' },
+            #  'GuestCounts' => { 'Count' => guest_count }, 
+              'RoomType' => { 'NumberOfUnits' => unit_count }
+            }
           }
       }
       end
@@ -40,14 +38,18 @@ module Sabre
     end
 
     def self.confirm(session, full_name)
-      client = Sabre.client('EndTransactionLLS1.4.1RQ.wsdl')
-      response = client.request(:end_transaction_rq, Sabre.request_header('1.4.1')) do
+      client = Sabre.client('EndTransactionLLS2.0.0RQ.wsdl')
+      response = client.request(:end_transaction_rq, Sabre.request_header('2.0.0', false)) do
         Sabre.namespaces(soap)
-        soap.header = session.header('Hotel Booking Confirmation','sabreXML','EndTransactionLLSRQ')
+        soap.header = session.header('End Transaction','sabreXML','EndTransactionLLSRQ')
         soap.body = {
-          'POS' => Sabre.pos,
-          'UpdatedBy' => { 'TPA_Extensions' => { 'Access' => { 'AccessPerson' => { 'GivenName' => full_name } } } },
-          'EndTransaction' => '', :attributes! => { 'EndTransaction' => { 'Ind' => 'true' } }
+          'EndTransaction' => '',#{ 'Email' => '',#{ 'Itinerary' => { 'PDF' => '', :attributes! => { 'PDF' => { 'Ind' => 'true' } } }, 
+                                             #:attributes! => { 'Itinerary' => { 'Ind' => 'true' } }
+                                             #}, 
+          #                        :attributes! => {'Email' => {'Ind' => 'true'} } 
+          #}, 
+          'Source' => '',
+          :attributes! => { 'EndTransaction' => { 'Ind' => 'true' }, 'Source' => { 'ReceivedFrom' => 'SWS TEST' } }
         }
       end
     end
