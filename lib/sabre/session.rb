b6a61cd276b6d6196131920db887fa102f316010
Andri Kurnia <andrikurnia@live.com>
2015-07-28 10:57:57 +0700

    update

--- lib/sabre/session.rb
+++ lib/sabre/session.rb
@@ -6,7 +6,7 @@ module Sabre
     attr_accessor :username, :password, :pcc, :ipcc, :binary_security_token, :ref_message_id, :domain, :conversation_id
 
     def initialize(params)
-      self.conversation_id       = params[:conversation_id] || nil
+      self.conversation_id       = params[:conversation_id] || 'possetrips-test'
       self.username              = params[:username] || Sabre.username
       self.password              = params[:password] || Sabre.password
       self.ipcc                  = params[:ipcc]     || Sabre.ipcc
